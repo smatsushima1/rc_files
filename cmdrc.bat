b3e4d93520582ba958860b4efb86a6e688d03915
smatsushima1 <smatsushima1@gmail.com>
2021-02-13 15:29:49 -0500

    add cmdrc.bat

---
+++ cmdrc.bat
@@ -0,0 +1,8 @@
+@echo off
+
+:: For bash equivalents = https://www.lemoda.net/windows/windows2unix/windows2unix.html
+doskey l=dir
+doskey cd=chdir
+doskey rm=delete
+doskey np=notepad
+doskey cat=type
